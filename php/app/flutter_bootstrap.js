4013a187b8804475592a47c95a7e62daed6ebaf5
Kran19 <karanmack5356@gmail.com>
2026-09-23 16:22:07 +0530

    fix: database configuration, auto-seeder, and production release v1.5.8

--- php/app/flutter_bootstrap.js
+++ php/app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4278762044" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2096450644" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
